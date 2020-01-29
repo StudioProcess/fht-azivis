d169b2b46450800e9ce39c94d4757b258689436b
Martin Leopold <m@martinleopold.com>
2020-01-29 14:19:40 +0100

    Proper size and units

--- app/main.js
+++ app/main.js
@@ -3,7 +3,11 @@ import { makeGUI } from './gui.js';
 import { SVG } from '../node_modules/@svgdotjs/svg.js/dist/svg.esm.js';
 import * as util from './util.js';
 
-const datasets = {};
+const config = {
+  W: 410, // mm
+  H: 276, // mm
+};
+
 const params = {
   'dataset': 'partner',
   'scale': 80,
@@ -11,6 +15,8 @@ const params = {
   'bg_color': '#ffffff',
   'strokeWidth': 0.1,
 };
+
+const datasets = {};
 
 (async function main() {
   
@@ -31,15 +37,16 @@ const params = {
   
   // draw svg
   function draw() {
-    let W = 1000, H = 1000;
-    let draw = SVG('svg').size(W, H);
+    let W = mm2pt(config.W), H = mm2pt(config.H);
+    let draw = SVG('svg').size(W + 'pt', H + 'pt');
+    draw.viewbox(0, 0, W, H); // now we can specify all values in pt, but don't have to write 'pt' all the time
     draw.clear();
     updateBG();
     
     let data = datasets[params.dataset];
     console.log(data);
     
-    draw.circle(5).center(W/2,H/2);
+    draw.circle(10).center(W/2, H/2);
     
     for (let d of data) {
       // console.log(d);
@@ -51,7 +58,7 @@ const params = {
       // 
       // if (Math.random() < 0.2)
       draw.line(W/2, H/2, p.x, p.y).stroke({ width:params.strokeWidth, color:params.color });
-      
+    
       draw.circle(1).center(p.x, p.y).fill(params.color);
       // if (Math.random() < 0.1)
         // draw.text(d.partner).move(p.x+3, p.y+1.5).attr({'font-family': 'system-ui', 'font-size':5, 'opacity':0.5})
@@ -79,7 +86,8 @@ const params = {
   
 })();
 
-
 export function updateBG() {
   document.querySelector('svg').style.backgroundColor = params.bg_color;
 }
+
+function mm2pt(mm) { return mm / 25.4 * 72; }
